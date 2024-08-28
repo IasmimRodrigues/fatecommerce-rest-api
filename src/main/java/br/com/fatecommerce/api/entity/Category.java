87e33aae5a170dee5bc517eb4b881548ebbe219f
Tamiris <tamiris.carvalho04@gmail.com>
2024-08-28 20:21:19 -0300

    crud finalizado

---
+++ src/main/java/br/com/fatecommerce/api/entity/Category.java
@@ -0,0 +1,35 @@
+package br.com.fatecommerce.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Category{
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private Long idCategory;
+
+    @Column(name = "name_category", nullable = false, length = 300, unique = true)
+    private String nameCategory;
+
+    @Column(name = "description_category", length = 1000)
+    private String descriptionCategory;
+
+}
+
+
+
+   
