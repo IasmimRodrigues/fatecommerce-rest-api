e7a3a5294fa0d4b92f01165a880ca95d8c77b979
Tamiris <tamiris.carvalho04@gmail.com>
2024-09-10 20:25:01 -0300

    name finalized

--- src/main/java/br/com/fatecommerce/api/service/ProductService.java
+++ src/main/java/br/com/fatecommerce/api/service/ProductService.java
@@ -16,64 +16,77 @@ import br.com.fatecommerce.api.repository.ProductRepository;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
- 
-    
+
     public Product saveProduct(Product product) {
         return productRepository.saveAndFlush(product);
     }
- 
+
     public List<Product> getInfoProducts() {
         return productRepository.findAll();
     }
+
     public HashMap<String, Object> deleteProduct(Long idProduct) {
-        Optional<Product> product =
-                Optional.ofNullable(productRepository.findById(idProduct).
-                        orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                                "Produto não encontrado!")));
- 
- 
+        Optional<Product> product = Optional.ofNullable(productRepository.findById(idProduct)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto não encontrado!")));
+
         productRepository.delete(product.get());
         HashMap<String, Object> result = new HashMap<String, Object>();
-        result.put("result", "Produto: " + product.get().getNameProduct() +  " excluído com sucesso!");
+        result.put("result", "Produto: " + product.get().getNameProduct() + " excluído com sucesso!");
         return result;
     }
+
     public Product findProductById(Long idProduct) {
         return productRepository.findById(idProduct)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Produto não encontrado!"));
     }
 
-    public Product updProduct(Product product){
-        if(findProductById(product.getIdProduct()) != null) {
+    public Product updProduct(Product product) {
+        if (findProductById(product.getIdProduct()) != null) {
             return productRepository.saveAndFlush(product);
-        }
-        else{
+        } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-            "Produto não encontrado!");
+                    "Produto não encontrado!");
         }
-        
+
     }
 
     public Optional<Product> findByEanProduct(String eanProduct) {
         return Optional.ofNullable(productRepository.findByEanProduct(eanProduct)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto com EAN " + eanProduct + " não encontrado!")));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto com EAN " + eanProduct + " não encontrado!")));
     }
 
-  
     public List<Product> findBySkuProduct(String skuProduct) {
-        if(skuProduct != null || !skuProduct.equals("")){
-        List<Product> products = productRepository.findBySkuProduct(skuProduct);
-        if (!products.isEmpty()) {
-            return products;
-        } else{ 
+        if (skuProduct != null || !skuProduct.equals("")) {
+            List<Product> products = productRepository.findBySkuProduct(skuProduct);
+            if (!products.isEmpty()) {
+                return products;
+            } else {
 
-throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto com SKU " + skuProduct + " não encontrado!");
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto com SKU " + skuProduct + " não encontrado!");
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum produto encontrado ");
         }
-    }else{
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum produto encontrado ");
+
     }
-       
-}}
 
- 
+    public List<Product> findByNameProduct(String nameProduct) {
+        if (nameProduct != null || !nameProduct.equals("")) {
+            List<Product> products = productRepository.findByNameProductIgnoreCaseContaining(nameProduct);
+            if (!products.isEmpty()) {
+                return products;
+            } else {
 
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Produto com SKU " + nameProduct + " não encontrado!");
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum produto encontrado ");
+        }
+
+    }
+}
